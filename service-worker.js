06671ce782998370981c5250473ad7bf33979e10
jack9211kimo-code <jack9211kimo@gmail.com>
2026-05-17 16:02:40 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='english-learning-v72-clean-vocab';
-const ASSETS=['./?v=72','./index.html','./manifest.json','./icon-192.svg','./icon-512.svg'];
+const CACHE='english-learning-v73-lists';
+const ASSETS=['./?v=73','./index.html','./manifest.json','./icon-192.svg','./icon-512.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{const c=r.clone();caches.open(CACHE).then(cache=>cache.put(e.request,c)).catch(()=>{});return r;}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));});
