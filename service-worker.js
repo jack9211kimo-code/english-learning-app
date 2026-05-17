16c89dec1d55eb5d540ab5eed44e8de525001fa5
jack9211kimo-code <jack9211kimo@gmail.com>
2026-05-17 14:01:03 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='english-learning-v4-pronunciation';
-const ASSETS=['./','./index.html','./manifest.json','./icon-192.svg','./icon-512.svg'];
+const CACHE='english-learning-v5-3000';
+const ASSETS=['./','./index.html','./wordbank.js','./manifest.json','./icon-192.svg','./icon-512.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));});
